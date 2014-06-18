5f79a005c44e2baebce0dba496cff5ba16e49133
unknown <bvikasvburman@gmail.com>
2014-06-19 00:52:04 +0530

    Add first file

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+     <html>
+          <head>
+               <title> Manjhi Samaj </title>
+               
+               <!-- <BASE HREF="http://www.bunnyindia.com/"> -->
+               <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+               <meta name="abstract" content="Manjhi Samaj, India">
+               <meta NAME="Contact" CONTENT="Manjhi Samaj / bvikasvburman@gmail.com"> 
+               <meta NAME="Robots" CONTENT="ALL"> 
+               <meta NAME="Robots" CONTENT="INDEX,FOLLOW"> 
+               <meta NAME="Revisit-After" CONTENT="7 Days"> 
+               <meta Content="Global" name="distribution"> 
+               <meta Content="Vikas Burman" name="author"> 
+               <meta Content="en-gb" name="language"> 
+               <meta Content="All" name="audience"> 
+               <meta content="General" name="rating"> 
+               <meta content="document" name="resource-type"> 
+               <meta name="save" content="history"> 
+               <meta name="description" content="Manjhi Samaj">
+               <meta name="keywords" content="Manjhi Samaj">
+
+               <!--<link REL="SHORTCUT ICON" HREF="images/dashark.png" />-->
+          </head>
+          <body>
+               <div style="position: relative; top:100px; left:350px;">
+                    <img src="images/coming_soon.gif" alt="Comming Soon" title="Manjhi Samaj" />
+               </div>
+          </body>
+     </html>
